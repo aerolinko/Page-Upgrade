2430091208a2cd091fd6d3bec51f1a064aadfc72
aerolinko <cesarst04@gmail.com>
2025-08-15 21:22:46 -0400

    uso de variables de entorno

--- page-upgrade/app/lib/db.ts
+++ page-upgrade/app/lib/db.ts
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'sys',
-    password: 'prueba123',
-    port: 3306,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
 });
 
 export async function getUser(username: string, password: string) {
