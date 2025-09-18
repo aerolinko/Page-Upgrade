fd22d35a76a883d871ffe3f96e15bc6007298b91
aerolinko <cesarst04@gmail.com>
2025-09-18 02:11:20 -0400

    fix fechas erróneas 3

--- page-upgrade/app/(sidenav)/distribuidores/page.tsx
+++ page-upgrade/app/(sidenav)/distribuidores/page.tsx
@@ -151,7 +151,7 @@ export default function LoginPage() {
                 role.cedula.toString().includes(lowerCaseFilter) ||
                 role.segundo_nombre?.toLowerCase().includes(lowerCaseFilter) ||
                 role.segundo_apellido?.toLowerCase().includes(lowerCaseFilter) ||
-                formatDateGMT4(role.fecha_correcta).toLowerCase().includes(lowerCaseFilter)
+                formatDateGMT4(role.fecha_contrato).toLowerCase().includes(lowerCaseFilter)
             );
         }
         return currentRoles;
