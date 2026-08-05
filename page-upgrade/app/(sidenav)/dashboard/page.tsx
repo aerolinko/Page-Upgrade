ecef54e243ba66fa6276fb4916449e6bdcb30ae8
aerolinko <cesarst04@gmail.com>
2026-08-05 19:05:47 -0400

    Typescript formatting fix

--- page-upgrade/app/(sidenav)/dashboard/page.tsx
+++ page-upgrade/app/(sidenav)/dashboard/page.tsx
@@ -206,13 +206,13 @@ export default function Dashboard() {
 
             setTotal3kg(
                 formattedData
-                    ? formattedData.reduce((acc, curr) => acc + (curr?.ventas_3kg || 0), 0)
+                    ? formattedData.reduce((acc: number, curr) => acc + (curr?.ventas_3kg || 0), 0)
                     : 0
             );
 
             setTotal6kg(
                 formattedData
-                    ? formattedData.reduce((acc, curr) => acc + (curr?.ventas_6kg || 0), 0)
+                    ? formattedData.reduce((acc: number, curr) => acc + (curr?.ventas_6kg || 0), 0)
                     : 0
             );
 
